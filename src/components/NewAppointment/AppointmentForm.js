df721ba4c6d71fb83b8dbcd9daeb2d710b7eb22d
Ralph Woiwode <rawoiwode@gmail.com>
2023-03-10 13:22:14 -0600

    Working on the appointment form logic to add a new appointment

--- src/components/NewAppointment/AppointmentForm.js
+++ src/components/NewAppointment/AppointmentForm.js
@@ -1,16 +1,67 @@
+import { useState } from "react";
 import "./AppointmentForm.css";
 
 const AppointmentForm = (props) => {
+  const [enteredDateTime, setEnteredDateTime] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
+  const [isSubmitEnabled, setIsSubmitEnabled] = useState(false);
+
+  const dateTimeChangeHandler = (event) => {
+    setEnteredDateTime(event.target.value);
+    enableSubmit();
+  };
+
+  const descriptionChangeHandler = (event) => {
+    setEnteredDescription(event.target.value);
+    enableSubmit();
+  };
+
+  const locationChangeHandler = (event) => {
+    setSelectedLocation(event.target.value);
+    enableSubmit();
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); // Don't let form submit and reload page in a default way
+
+    const appointmentData = {
+      dateTime: new Date(enteredDateTime),
+      location: selectedLocation,
+      description: enteredDescription,
+    };
+
+    console.log(appointmentData);
+  };
+
+  const enableSubmit = () => {
+    const locationValid = selectedLocation !== "";
+    const dateTimeValid = enteredDateTime !== "";
+    const descriptionValid = enteredDescription !== "";
+    setIsSubmitEnabled(locationValid && dateTimeValid && descriptionValid);
+  };
+
+  // TODO: This is running on every change; need to figure out a better way
+  let minDate = new Date().toISOString();
+  console.log(minDate);
+  minDate = minDate.substring(0, minDate.length - 8); // Remove seconds through the z
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-appointment__inputs">
         <div>
           <label>DATE & TIME</label>
-          <input type="datetime-local" />
+          <input
+            min={minDate}
+            onChange={dateTimeChangeHandler}
+            required
+            type="datetime-local"
+          />
         </div>
         <div>
           <label>LOCATION</label>
-          <select>
+          <select id="locations" onChange={locationChangeHandler} required>
+            <option value="">Please select a city</option>
             <option value="London">London</option>
             <option value="Orlando">Orlando</option>
             <option value="Portland">Portland</option>
@@ -20,11 +71,21 @@ const AppointmentForm = (props) => {
         </div>
         <div>
           <label>DESCRIPTION</label>
-          <textarea />
+          <textarea
+            minLength={4}
+            maxLength={256}
+            onChange={descriptionChangeHandler}
+            placeholder="Please enter a description"
+            required
+            rows={4}
+            spellCheck={true}
+          />
         </div>
       </div>
       <button type="button">Cancel</button>
-      <button type="submit">Add Appointment</button>
+      <button type="submit" disabled={!isSubmitEnabled}>
+        Add Appointment
+      </button>
     </form>
   );
 };
